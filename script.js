71d8cb0e015c2e6c78022814cdf0230d1a27b8db
florianhennel <hennelflori@gmail.com>
2023-03-29 17:33:49 +0200

    creating setBoard function

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
-class Piece{
-    alive = true;
-    constructor(color,piece) {
-        this.color = color;
-        this.piece = piece;
+document.addEventListener("DOMContentLoaded", main);
+function main() {
+    class Coordinate {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
     }
-
-}
-class Pawn{
-    constructor(color,piece){
-        super(color,piece);
+  }
+  class Piece {
+    constructor(color, piece, coordinate) {
+      this.color = color;
+      this.piece = piece;
+      this.coordinate = coordinate;
     }
-    Moves() {
-        if(color="white"){}
-        else{}
-    }
-}
-pieceArray = [];
-for (let index = 0; index < 8; index++) {
-    p = new Piece(white,pawn);
-    pieceArray[index] = p;
-}
-const babuk = [
-    ["","","","","","","",""],
-    ["","","","","","","",""],
-    ["","","","","","","",""],
-    ["","","","","","","",""],
-    ["","","","","","","",""],
-    ["","","","","","","",""],
-    ["","","","","","","",""],
-    ["","","","","","","",""]
-]
+  }
+  const squares = document.querySelectorAll(".squares");
+  const btn = document.getElementById("btn");
+  const types = ["pawn", "rook", "knight", "bishop", "queen", "king"];
+  let pieces = [[],[],[],[],[],[],[],[]];
+  setBoard();
 
-function setBoard(){
-
+  squares.forEach((square) => {
+    square.addEventListener("click", clickPiece);
+  });
+  function clickPiece() {
+    const index = this.getAttribute("cellindex");
+    squares[index - 1].textContent = "Here!";
+  }
+  function setBoard() {
+    types.forEach((element) => {
+      switch (element) {
+        case "pawn":
+          for (let i = 1; i <= 8; i++) {
+            let white = new Piece("white","pawn",new Coordinate(6,i));
+            let  black = new Piece("black","pawn",new Coordinate(1,i));
+            pieces[6][i] = white;
+            pieces[1][i] = black;
+          }
+      }
+    });
+    pieces = [
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+    ];
+  }
 }
